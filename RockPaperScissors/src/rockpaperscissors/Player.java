df2a1172e859c4b2cb32c4af6465c5e177a1f7f5
bombenten <bombenten@hotmail.com>
2020-11-23 02:35:18 +0700

    Create method in class player

--- RockPaperScissors/src/rockpaperscissors/Player.java
+++ RockPaperScissors/src/rockpaperscissors/Player.java
@@ -5,14 +5,37 @@ package rockpaperscissors;
  * @author Bomber
  */
 public class Player {
-    private final int chance = 3;
+    private final int Chance = 3;
     private int currentChance;
     private String name;
     
     public Player(){
-        this.currentChance = chance;
+        this.currentChance = Chance;
     }
     
-    
+    public void setName(String name) {
+        
+    }
+    public String getName(){
+        return name;
+    }
+    public int getChance(){
+        return Chance;
+    }
+    public int getCurrentChance(){
+        return currentChance;
+    }
+    public void useOption(int choice, Player anotherPlayer){
+        
+    }
+    private void checkDmg(Player player){
+        
+    }
+    public void heal(){
+        
+    }
+    public String option(int choice){
+        return 0;
+    }
     
 }
